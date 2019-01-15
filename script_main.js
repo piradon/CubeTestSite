1e22928836e6ddc9dd51e6316f8dc16fb9d20984
pirado <apiotre@gmail.com>
2019-01-15 20:01:11 +0100

    code review and files format

--- script_main.js
+++ script_main.js
@@ -1,33 +1,30 @@
+const whiteColor = "rgb(255, 255, 255)";
+const blueColor = "rgb(3, 79, 132)";
+const redColor = "rgb(189, 61, 58)";
+const orangeColor = "rgb(228, 122, 46)";
+const greenColor = "rgb(0, 128, 0)";
+const yellowColor = "rgb(236, 219, 84)";
 
-var whiteColor="rgb(255, 255, 255)";
-var blueColor="rgb(3, 79, 132)";
-var redColor="rgb(189, 61, 58)";
-var orangeColor="rgb(228, 122, 46)";
-var greenColor="rgb(0, 128, 0)";
-var yellowColor="rgb(236, 219, 84)";
+const colorArray = [
+  whiteColor,
+  blueColor,
+  redColor,
+  orangeColor,
+  greenColor,
+  yellowColor
+];
 
-var i=0;
-
-var colorArray = [whiteColor, blueColor, redColor, orangeColor, greenColor, yellowColor];
-
-function changeColor (el) {
-   
-    const myStyle = getComputedStyle(el);
-    const currentColor=myStyle.backgroundColor;
-    var indexOfColor=colorArray.indexOf(currentColor);
-    
-    if(indexOfColor<colorArray.length-1){
-        indexOfColor=indexOfColor+1;
-        el.style.background=colorArray[indexOfColor];
-    }
-    else {
-        el.style.background=colorArray[0];
-    }
-    
-  
+function changeColor(el) {
+  const myStyle = getComputedStyle(el);
+  const currentColor = myStyle.backgroundColor;
+  el.style.background = getNextColor(currentColor);
 }
 
-
-
-
-
+function getNextColor(currentColor) {
+  let colorIndex = colorArray.indexOf(currentColor);
+  const nextColor =
+    colorIndex < colorArray.length - 1
+      ? colorArray[colorIndex + 1]
+      : colorArray[0];
+  return nextColor;
+}
